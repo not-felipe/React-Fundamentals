db6c354d3494c6046b2e186b78e5b0e0ad7ea58d
not-felipe <felipeam@id.uff.br>
2023-05-19 20:04:24 -0300

    Aprendendo Estilização no React

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import Title from "./components/Title/";
 import { Subtitle } from "./components/Subtitle";
 import StatusText from "./components/StatusText";
+import styles from "./App.module.css"
 
 export default function App() {
   return (
-    <div
-      style={{
-        backgroundColor: "#2c2c2d",
-        display: "grid",
-        minHeight: "100vh",
-        placeContent: "center",
-        textAlign: "center",
-      }}
-    >
+    <div className={styles.app}>
       <Title />
       <Subtitle />
       <StatusText />
     </div>
-  );
+  )
 }
